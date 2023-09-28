7cf9ef4bdde39d15fb11d0a139f1b79ccf3472ba
angelcepeda <jangelcepeda@protonmail.com>
2023-09-28 14:06:33 -0700

    full repo proyect with front

---
+++ Web-app-front/cc-vue-todo-2023/src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+import axios from 'axios'
+
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+
+const todos_asc = computed(() => todos.value.sort((a,b) =>{
+	return a.createdAt - b.createdAt
+}))
+
+const addTodo = () => {
+  if (input_content.value.trim() === '') {
+    return
+  }
+
+  const newTodo = {
+    content: input_content.value,
+    done: false,
+  }
+
+  axios.post('/todos', newTodo)
+    .then(response => {
+      todos.value.push(response.data)
+    })
+    .catch(error => {
+      console.error('Error adding todo:', error)
+    })
+}
+
+const removeTodo = (todo) => {
+  axios.delete(`/todos/${todo.id}`)
+    .then(response => {
+      todos.value = todos.value.filter((t) => t !== todo)
+    })
+    .catch(error => {
+      console.error('Error deleting todo:', error)
+    })
+}
+
+onMounted(() => {
+	name.value = localStorage.getItem('name') || ''
+	todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+</script>
+
+<template>
+	<main class="app">
+		
+		<section class="greeting">
+			<h2 class="title">
+				To-do
+			</h2>
+		</section>
+
+		<section class="create-todo">
+			<h3>Create a To-Do</h3>
+
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>What's on your todo list?</h4>
+				<input 
+					type="text" 
+					name="content" 
+					id="content" 
+					placeholder="e.g. make a video"
+					v-model="input_content" />
+			
+				<input type="submit" value="Add todo" />
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<h3>TODO LIST</h3>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="todo.done" />
+						<span :class="`bubble ${
+							todo.category == 'business' 
+								? 'business' 
+								: 'personal'
+						}`"></span>
+					</label>
+
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" />
+					</div>
+
+					<div class="actions">
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
+</template>
