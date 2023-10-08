c457c51df665f4361b1cdb446c1befaa7b9e98f5
angelcepeda <jangelcepeda@protonmail.com>
2023-10-08 14:13:55 -0700

    nginx conf, playbook

--- myapp/app.js
+++ myapp/app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "http://localhost:8080"
+  origin: "*"
 }));
 
 app.use('/', indexRouter);
