ff419e80f5db07f4b838595ddc3a6b4ec9dc229b
angelcepeda <jangelcepeda@protonmail.com>
2023-10-10 19:42:46 -0700

    aberraciones

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 const controller = require('../controllers/tasks');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welome to do tasks' });
-});
 router.get('/',controller.list);
 
 module.exports = router;
